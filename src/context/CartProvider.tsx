450586f536ad7c8dcdbf8479c42f370a476ac171
Martín <martincalderon92@outlook.com>
2024-07-10 23:37:54 -0300

    chore: Add CartProvider context for managing cart state

---
+++ src/context/CartProvider.tsx
@@ -0,0 +1,102 @@
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState
+} from 'react'
+import { Product } from '../common/interfaces'
+
+interface CartContextValue {
+  cartItems: Product[]
+  addToCart: (product: Product) => void
+  removeFromCart: (productId: number) => void
+  clearCart: () => void
+  getTotal: () => number
+}
+
+const CartContext = createContext<CartContextValue | undefined>(undefined)
+
+export const useCart = () => {
+  const context = useContext(CartContext)
+
+  if (!context) throw new Error('useCart must be used within a CartProvider')
+
+  return context
+}
+
+interface CartProvider {
+  children: ReactNode
+}
+
+const CartProvider = ({ children }: CartProvider) => {
+  const [cartItems, setCartItems] = useState<Product[]>([])
+
+  useEffect(() => {
+    const savedCartItems = localStorage.getItem('cartItems')
+
+    if (savedCartItems) setCartItems(JSON.parse(savedCartItems))
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems))
+  }, [cartItems])
+
+  const addToCart = useCallback(
+    (product: Product) => {
+      setCartItems((prevItems) => {
+        const existingCartItem = prevItems.find(
+          (item) => item.id === product.id
+        )
+
+        if (existingCartItem) {
+          return prevItems.map((item) =>
+            item.id === product.id
+              ? { ...item, amount: item.amount + product.amount }
+              : item
+          )
+        } else {
+          return [...prevItems, product]
+        }
+      })
+    },
+    [setCartItems]
+  )
+
+  const removeFromCart = useCallback(
+    (productId: number) => {
+      setCartItems((prevItems) =>
+        prevItems.filter((item) => item.id !== productId)
+      )
+    },
+    [setCartItems]
+  )
+
+  const clearCart = useCallback(() => {
+    setCartItems([])
+  }, [setCartItems])
+
+  const getTotal = useCallback(() => {
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.amount,
+      0
+    )
+  }, [cartItems])
+
+  const value: CartContextValue = useMemo(
+    () => ({
+      cartItems,
+      addToCart,
+      removeFromCart,
+      clearCart,
+      getTotal
+    }),
+    [cartItems, addToCart, removeFromCart, clearCart, getTotal]
+  )
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>
+}
+
+export default CartProvider
