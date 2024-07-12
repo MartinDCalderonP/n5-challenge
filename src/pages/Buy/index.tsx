c3353bf795665bd7f6d6d3778fb24000cf7ad049
Martín <martincalderon92@outlook.com>
2024-07-12 14:17:26 -0300

    feat: add Buy page

--- src/pages/Buy/index.tsx
+++ src/pages/Buy/index.tsx
@@ -1,5 +1,19 @@
+import styles from './styles.module.scss'
+import { Link } from 'react-router-dom'
+import useCart from '../../hook/useCart'
+import { currencyFormatter } from '../../utils'
+
 const Buy = () => {
-  return <div>Buy</div>
+  const { getTotal } = useCart()
+
+  const totalPrice = currencyFormatter.format(getTotal())
+
+  return (
+    <main className={styles.buyContainer}>
+      <h1> El precio final de su compra es de: {totalPrice}</h1>
+      <Link to='/'>¿Desea agregar algún otro producto?</Link>
+    </main>
+  )
 }
 
 export default Buy
