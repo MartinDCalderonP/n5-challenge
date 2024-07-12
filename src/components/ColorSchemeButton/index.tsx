2604b09315bea6fcae99e65c77468e29627ffc2a
Martín <martincalderon92@outlook.com>
2024-07-12 16:41:46 -0300

    feat: create ColorSchemeButton component

---
+++ src/components/ColorSchemeButton/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import styles from './styles.module.scss'
+import { FaMoon, FaSun } from 'react-icons/fa'
+
+type ColorScheme = 'light' | 'dark'
+
+const ColorSchemeButton = () => {
+  const currentColorScheme =
+    document.documentElement.getAttribute('color-scheme')
+
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    currentColorScheme as ColorScheme
+  )
+
+  const toggleColorScheme = () => {
+    const newColorScheme = colorScheme === 'light' ? 'dark' : 'light'
+    setColorScheme(newColorScheme)
+    document.documentElement.setAttribute('color-scheme', newColorScheme)
+  }
+
+  return (
+    <button className={styles.colorSchemeButton} onClick={toggleColorScheme}>
+      {colorScheme === 'dark' ? <FaSun /> : <FaMoon />}
+    </button>
+  )
+}
+
+export default ColorSchemeButton
