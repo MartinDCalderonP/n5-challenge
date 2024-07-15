137af9b0af00f76289356de89c70471c475ede8d
Martín <martincalderon92@outlook.com>
2024-07-15 20:56:29 -0300

    chore: add CartButton test suite with render and click tests

---
+++ src/__test__/components/CartButton.test.tsx
@@ -0,0 +1,66 @@
+import { fireEvent, render } from '@testing-library/react'
+import CartButton from '../../components/CartButton'
+import { describe, test, vi, expect, beforeEach } from 'vitest'
+
+describe('CartButton', () => {
+  beforeEach(() => {
+    vi.resetAllMocks()
+    vi.clearAllMocks()
+  })
+
+  test('renders CartButton and shows the correct cart item count', async () => {
+    const mockUseCart = await import('../../hook/useCart')
+
+    mockUseCart.default = vi.fn().mockReturnValue({
+      cartItems: [{ id: 1 }, { id: 2 }]
+    })
+
+    const toggleSidebar = vi.fn()
+
+    const { getByRole, getByTestId } = render(
+      <CartButton toggleSidebar={toggleSidebar} />
+    )
+
+    const cartButton = getByRole('button')
+    const productCount = getByTestId('product-count')
+
+    expect(cartButton).toBeInTheDocument()
+    expect(productCount).toBeInTheDocument()
+  })
+
+  test('calls toggleSidebar when the button is clicked', async () => {
+    const mockUseCart = await import('../../hook/useCart')
+
+    mockUseCart.default = vi.fn().mockReturnValue({
+      cartItems: [{ id: 1 }, { id: 2 }]
+    })
+
+    const toggleSidebar = vi.fn()
+
+    const { getByRole } = render(<CartButton toggleSidebar={toggleSidebar} />)
+
+    const cartButton = getByRole('button')
+
+    fireEvent.click(cartButton)
+
+    expect(toggleSidebar).toHaveBeenCalled()
+  })
+
+  test('does not show the item count when there are no items in the cart', async () => {
+    const mockUseCart = await import('../../hook/useCart')
+
+    mockUseCart.default = vi.fn().mockReturnValue({
+      cartItems: []
+    })
+
+    const toggleSidebar = vi.fn()
+
+    const { queryByTestId } = render(
+      <CartButton toggleSidebar={toggleSidebar} />
+    )
+
+    const productCount = queryByTestId('product-count')
+
+    expect(productCount).not.toBeInTheDocument()
+  })
+})
