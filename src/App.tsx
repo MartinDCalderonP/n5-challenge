53efb92764bd317c130e1f5f620efbf48700255b
Martín <martincalderon92@outlook.com>
2024-07-16 18:15:06 -0300

    refactor: update App file & ProductsList component to use ProductsListProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProductsListProvider from './context/ProductsListProvider.tsx'
 import CartProvider from './context/CartProvider.tsx'
 import Home from './pages/Home/index.tsx'
 import Buy from './pages/Buy/index.tsx'
@@ -6,17 +7,19 @@ import AddProduct from './pages/AddProduct/index.tsx'
 
 const App = () => {
   return (
-    <CartProvider>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/buy' element={<Buy />} />
-          <Route path='/add-product' element={<AddProduct />} />
+    <ProductsListProvider>
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/buy' element={<Buy />} />
+            <Route path='/add-product' element={<AddProduct />} />
 
-          <Route path='*' element={<Home />} />
-        </Routes>
-      </Router>
-    </CartProvider>
+            <Route path='*' element={<Home />} />
+          </Routes>
+        </Router>
+      </CartProvider>
+    </ProductsListProvider>
   )
 }
 
--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -1,13 +1,15 @@
 import ProductListItem from './ProductListItem'
-import data from './products.json'
 import styles from './styles.module.scss'
+import useProductsList from '../../hook/useProductsList'
 
 const ProductsList = () => {
+  const { productsList } = useProductsList()
+
   return (
     <article>
       <h2>Products List</h2>
       <ul className={styles.productsList}>
-        {data.products.map((product) => (
+        {productsList.map((product) => (
           <ProductListItem
             key={product.id}
             id={product.id}
