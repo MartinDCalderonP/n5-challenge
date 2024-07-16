cd079aa9e0cc1196101a191a9d752c59ba6eb410
Martín <martincalderon92@outlook.com>
2024-07-16 19:01:32 -0300

    refactor: add capitalizeFirstLetter test in utils/index.test.ts

--- src/__test__/utils/index.test.ts
+++ src/__test__/utils/index.test.ts
@@ -1,5 +1,14 @@
-import { currencyFormatter } from '../../utils'
+import { capitalizeFirstLetter, currencyFormatter } from '../../utils'
 import { describe, expect, test } from 'vitest'
+
+describe('capitalizeFirstLetter', () => {
+  test('should capitalize the first letter of a string', () => {
+    const string = 'hello world'
+    const capitalizedString = capitalizeFirstLetter(string)
+
+    expect(capitalizedString).toBe('Hello world')
+  })
+})
 
 describe('currencyFormatter', () => {
   test('should be an instance of Intl.NumberFormat', () => {
