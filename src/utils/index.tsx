e7a5efb4e05569e6c21bdeec9737a141041c7afc
Martín <martincalderon92@outlook.com>
2024-07-16 19:01:22 -0300

    refactor: reorder functions in utils/index.tsx

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,8 +1,8 @@
+export const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
 export const currencyFormatter = new Intl.NumberFormat('es-AR', {
   style: 'currency',
   currency: 'ARS'
 })
-
-export const capitalizeFirstLetter = (str: string) => {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
