acfbac5e7c7b87c7b225c937c506cdcca5f58c87
Martín <martincalderon92@outlook.com>
2024-07-16 20:56:44 -0300

    test: add existing product error message test for AddProduct page

--- src/__test__/pages/AddProduct.test.tsx
+++ src/__test__/pages/AddProduct.test.tsx
@@ -125,4 +125,37 @@ describe('AddProduct', () => {
       expect(successMessage).toBeInTheDocument()
     })
   })
+
+  test('should show an error message if the product already exists', async () => {
+    const mockUseProductsList = await import('../../hooks/useProductsList')
+
+    vi.spyOn(mockUseProductsList, 'default').mockReturnValue({
+      addProduct: vi.fn().mockReturnValue('Product already exists'),
+      productsList: [{ id: 1, name: 'Product 1', price: 100, amount: 0 }]
+    })
+
+    const { getByLabelText, getByRole, queryByText } = renderWithProviders(
+      <AddProduct />
+    )
+
+    const nameInput = getByLabelText(/Nombre:/i)
+    const priceInput = getByLabelText(/Precio:/i)
+    const addButton = getByRole('button', { name: /Añadir Producto/i })
+
+    fireEvent.change(nameInput, { target: { value: 'Product 1' } })
+    fireEvent.change(priceInput, { target: { value: '100' } })
+
+    fireEvent.click(addButton)
+
+    await waitFor(() => {
+      expect(mockUseProductsList.default().addProduct).toHaveBeenCalledWith({
+        name: 'Product 1',
+        price: 100
+      })
+
+      const errorMessage = queryByText('Producto ya existe')
+
+      expect(errorMessage).toBeInTheDocument()
+    })
+  })
 })
