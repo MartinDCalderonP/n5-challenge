5790687de9795af4426b09149095d548349e7739
Martín <martincalderon92@outlook.com>
2024-07-16 13:49:18 -0300

    test: add unit tests for ProductsList component

---
+++ src/__test__/components/ProductsList.test.tsx
@@ -0,0 +1,29 @@
+import ProductsList from '../../components/ProductsList'
+import renderWithProviders from '../../utils/renderWithProviders'
+import { describe, expect, test } from 'vitest'
+import data from '../../components/ProductsList/products.json'
+import { currencyFormatter } from '../../utils'
+
+describe('ProductsList', () => {
+  test('renders a list of products', () => {
+    const { getAllByText, getByText } = renderWithProviders(<ProductsList />)
+
+    const productsListTitle = getByText('Products List', { selector: 'h2' })
+
+    expect(productsListTitle).toBeInTheDocument()
+
+    for (const product of data.products) {
+      const productNameText = new RegExp(product.name, 'i')
+      const productPriceText = currencyFormatter
+        .format(product.price)
+        .replace(/\$/g, '')
+        .replace(/\s/g, '')
+
+      const productName = getByText(productNameText)
+      const productPrice = getAllByText(new RegExp(productPriceText, 'i'))
+
+      expect(productName).toBeInTheDocument()
+      expect(productPrice.length).toBeGreaterThan(0)
+    }
+  })
+})
