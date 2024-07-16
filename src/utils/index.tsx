691d69bbd9eaf75e7ba40032107d1deb13f1acdf
Martín <martincalderon92@outlook.com>
2024-07-16 17:48:33 -0300

    refactor: add capitalizeFirstLetter utility function

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,3 +2,7 @@ export const currencyFormatter = new Intl.NumberFormat('es-AR', {
   style: 'currency',
   currency: 'ARS'
 })
+
+export const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
